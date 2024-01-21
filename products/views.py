5e1588921aa003c13e6f6df78b2615cf9e9df585
mechanovkirill <mechanovkirill@gmail.com>
2024-01-21 09:03:20 +0400

    Simple API created.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
+from rest_framework import permissions, viewsets
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows get current product prices.
+    """
+    queryset = Product.objects.all().order_by('name')
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
